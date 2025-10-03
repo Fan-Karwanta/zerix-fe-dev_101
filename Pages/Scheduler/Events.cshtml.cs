89dfd66ba09ad08e1c6ef94bb58d35facd275680
fankarwanta <fankarwanta@gmail.com>
2025-10-03 15:56:38 +0800

    improvements oct 3

---
+++ Pages/Scheduler/Events.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zerix_fe_dev.Pages.Scheduler
+{
+    public class EventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
