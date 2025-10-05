3ae30267b2b2968db45bdd620612f05cb5212705
Fan Try <fankarwanta@gmail.com>
2025-10-05 15:03:36 +0800

    corrections oct 5

--- Pages/Scheduler/user_view/Events.cshtml.cs
+++ Pages/Scheduler/user_view/Events.cshtml.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace zerix_fe_dev.Pages.Scheduler
+namespace zerix_fe_dev.Pages.Scheduler.user_view
 {
     public class EventsModel : PageModel
     {
