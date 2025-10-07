0473087f7c06a16ef2832396e4d83656e7085cc3
fankarwanta <fankarwanta@gmail.com>
2025-10-07 13:33:26 +0800

    improvements oct 7

---
+++ Pages/Scheduler/user_view/EditInformation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zerix_fe_dev.Pages.Scheduler.user_view
+{
+    public class EditInformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
