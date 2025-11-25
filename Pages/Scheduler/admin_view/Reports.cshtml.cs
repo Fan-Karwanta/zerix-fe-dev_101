a7e6a5e25a26eb7936509b664eeda36f85ec711a
Fan Try <fankarwanta@gmail.com>
2025-11-26 06:18:11 +0800

    some fix

---
+++ Pages/Scheduler/admin_view/Reports.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zerix_fe_dev.Pages.Scheduler.admin_view
+{
+    public class ReportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
